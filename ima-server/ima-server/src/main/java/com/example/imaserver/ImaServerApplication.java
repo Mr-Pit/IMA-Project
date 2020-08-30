25e58b11a4d08e0e355af8665d1167a86fae0da4
Pitney A. Jules <43119655+Mr-Pit@users.noreply.github.com>
2020-08-30 17:35:05 -0400

    Backend Installed

---
+++ ima-server/ima-server/src/main/java/com/example/imaserver/ImaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.imaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaServerApplication.class, args);
+	}
+
+}
---
+++ ima-server/ima-server/src/test/java/com/example/imaserver/ImaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
